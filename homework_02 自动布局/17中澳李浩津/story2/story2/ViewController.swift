d77a07f1da4a3794d0ef568bf67b19b679c9e4f7
Larrisa <1393317161@qq.com>
2019-09-19 13:44:07 +0800

    提交作业_李浩津

---
+++ homework_02 自动布局/17中澳李浩津/story2/story2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  story2
+//
+//  Created by Apple on 2019/9/17.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
