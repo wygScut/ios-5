502b98d697ceff9377bd203535da1d7a831c44dd
WindyZY <2461607010@qq.com>
2019-09-22 16:22:59 +0800

    周瑶的第一次作业（内有截图）

---
+++ zyy.174.SSE.SCUT/ZYY-demo1(作业）/ZYY-demo1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ZYY-demo1
+//
+//  Created by Apple on 2019/9/10.
+//  Copyright © 2019 ZYY.4.17.SE.SCUT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
